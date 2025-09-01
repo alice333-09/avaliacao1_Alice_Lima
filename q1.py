a6a9ec6465778f69eb5c02465b7a12992c7000a6
almdneto <airtinpvp@gmail.com>
2025-09-01 11:58:57 -0300

    correção

--- q1.py
+++ q1.py
@@ -1,6 +1,7 @@
 peso= float(input("digite seu peso em KG: "))
 altura=float(input("digite sua altura em metros: ")) 
-IMC= (peso/altura * altura)
+IMC= peso/(altura * altura)
+print(IMC)
 if IMC < 18.5:
     print("Abaixo do peso")
 if 18.5 <= IMC < 25:
--- q2.py
+++ q2.py
@@ -1,3 +1,4 @@
 alunos= input("Quantos alunos existem na turma: ")
+soma=0
 while alunos > 0:
     input("Qual sua nota: ")
