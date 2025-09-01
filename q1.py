de6f26b72f526c4d1429a3178fa5be4d019b8cd5
almdneto <airtinpvp@gmail.com>
2025-09-01 11:45:38 -0300

    resoluções

---
+++ q1.py
@@ -0,0 +1,11 @@
+peso= float(input("digite seu peso em KG: "))
+altura=float(input("digite sua altura em metros: ")) 
+IMC= (peso/altura * altura)
+if IMC < 18.5:
+    print("Abaixo do peso")
+if 18.5 <= IMC < 25:
+    print("Peso normal")
+if 25 <= IMC < 30:
+    print("Sobrepeso")
+elif IMC >= 30:
+    print("obesidade")
---
+++ q2.py
@@ -0,0 +1,3 @@
+alunos= input("Quantos alunos existem na turma: ")
+while alunos > 0:
+    input("Qual sua nota: ")
